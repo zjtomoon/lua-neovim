2bde2c952768dc66e462b02f77ce019adfd68d1d
zjtomoon <zjtomoon@outlook.com>
2022-02-04 21:11:08 +0800

    default commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,75 +1,90 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require('packer').startup(
+  {
+    function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -------------------------- plugins -------------------------------------------
+    -------------------------- plugins -------------------------------------------
 
-  -- nvim-tree
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons'
+    -- nvim-tree
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons'
+    }
+    -- bufferline
+    use {
+      'akinsho/bufferline.nvim',
+      requires = 'kyazdani42/nvim-web-devicons'
+    }
+    -- treesitter
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+    }
+
+    -- Comment
+    use 'numToStr/Comment.nvim'
+
+    -- nvim-autopairs
+    use 'windwp/nvim-autopairs'
+    -------------------------- lsp -------------------------------------------
+
+    -- lspconfig
+    use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
+    -- nvim-cmp
+    use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
+    use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
+    use 'hrsh7th/cmp-path'     -- { name = 'path' }
+    use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
+    use 'hrsh7th/nvim-cmp'
+    -- vsnip
+    use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
+    use 'hrsh7th/vim-vsnip'
+    use 'rafamadriz/friendly-snippets'
+    -- lspkind
+    use 'onsails/lspkind-nvim'
+    -- use 'github/copilot.vim'
+
+    use 'simrat39/rust-tools.nvim'
+    --------------------------- colorscheme ------------------------------------
+
+    -- gruvbox
+    use {
+      "ellisonleao/gruvbox.nvim",
+      requires = {"rktjmp/lush.nvim"}
+    }
+    -- zephyr
+    use 'glepnir/zephyr-nvim'
+    -- nord
+    use 'shaunsingh/nord.nvim'
+    -- onedark
+    use 'ful1e5/onedark.nvim'
+    ------------------------------------
+    --
+    -- which-key
+    -- use 'folke/which-key.nvim'
+
+    -- telescope
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/plenary.nvim'}}
+    }
+    
+    end,
+        config = {
+      max_jobs = 16,
+      git = {
+        --修改这里可以切换加速的节点
+        default_url_format = "https://hub.fastgit.org/%s"
+      },
+      display = {
+        open_fn = function()
+          return require("packer.util").float({border = "single"})
+        end
+      }
+    }
   }
-  -- bufferline
-  use {
-    'akinsho/bufferline.nvim',
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  -- treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-
-  -- Comment
-  use 'numToStr/Comment.nvim'
-
-  -- nvim-autopairs
-  use 'windwp/nvim-autopairs'
-  -------------------------- lsp -------------------------------------------
-
-  -- lspconfig
-  use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
-  -- nvim-cmp
-  use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
-  use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
-  use 'hrsh7th/cmp-path'     -- { name = 'path' }
-  use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
-  use 'hrsh7th/nvim-cmp'
-  -- vsnip
-  use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
-  use 'hrsh7th/vim-vsnip'
-  use 'rafamadriz/friendly-snippets'
-  -- lspkind
-  use 'onsails/lspkind-nvim'
-  -- use 'github/copilot.vim'
-
-  use 'simrat39/rust-tools.nvim'
-  --------------------------- colorscheme ------------------------------------
-
-  -- gruvbox
-  use {
-    "ellisonleao/gruvbox.nvim",
-    requires = {"rktjmp/lush.nvim"}
-  }
-  -- zephyr
-  use 'glepnir/zephyr-nvim'
-  -- nord
-  use 'shaunsingh/nord.nvim'
-  -- onedark
-  use 'ful1e5/onedark.nvim'
-  ------------------------------------
-  --
-  -- which-key
-  -- use 'folke/which-key.nvim'
-
-  -- telescope
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/plenary.nvim'}}
-  }
-  
-end
 )
 
